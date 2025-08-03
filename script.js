1777ab75810ca956d8ba7b0fd3b719e1d3b33e03
shikhar250 <shikhar.maurya2504@gmail.com>
2025-08-04 00:01:18 +0530

    Deleted Everything

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-const heading = document.createElement("h1");
+const btn = document.createElement("btn");
 
-heading.innerHTML = "Welcome to the Website!"
+btn.innerHTML = "Log In"
+document.body.appendChild(btn);
+const body = document.body;
 
-document.body.appendChild(heading);
+body.style.backgroundColor = "#212121";
+body.style.display = "flex";
+body.style.justifyContent = "center";
+body.style.alignItems = "center";
 
-// heading.style.padding = "10px";
+
+// btn.style.padding = "10px";
 const styles = {
-
+    backgroundColor: '#661d1dff',
+    fontWeight: '800',
+    fontSize: '20px',
+    border: 'none',
+    borderRadius: '10px',
+    height: '40px',
+    width: '100px',
     color: 'crimson',
-    border: '1px solid black'
+    border: '1px solid black',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer'
 }
 
-Object.assign(heading.style, styles)
+Object.assign(btn.style, styles)
+
+
+let data = {
+    userID: "35e55dp",
+    greet: function() {
+        console.log(`User ${this.userID} Logged In`);
+        console.log(this);
+        
+    }
+}
+
+data.greet()
